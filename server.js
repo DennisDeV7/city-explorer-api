d39681e9bdfbebf60c5f1dca4deb311ff2cf0042
Dennis DeVries <7dennisdv7@gmail.com>
2022-06-22 19:36:05 -0400

    adding complete solution for lab 07

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ console.log('our first server');
 const express = require('express');
 require('dotenv').config();
 let data = require('./data/weather.json')
-console.log(data);
+// console.log(data);
 
 const cors = require('cors');
 //USE
@@ -21,25 +21,28 @@ const PORT = process.env.PORT || 3002;
 //Routes
 app.get('/weather', (request, response) => {
   try {
-    let latFromRequest = request.query.lat;
-    let lonFromRequest = request.query.lon;
-    let searchQueryRequest = request.query.searchQuery;
-    let dataToGroom = data.find(weather => weather.lat === latFromRequest && weather.lat === lonFromRequest && weather.searchQuery === searchQueryRequest);
-    let dataToSend = new Forecast(dataToGroom);
+    const searchQuery = request.query.searchQuery;
+    console.log(searchQuery);
+    let dataToGroom = data.find(weather => weather.city_name.toLowerCase() === searchQuery.toLowerCase());
+    console.log(dataToGroom);
+    let dataToSend = dataToGroom.data.map(dayObj => new Forecast(dayObj));
     response.send(dataToSend);
+    console.log(datatoSend);
   } catch (error) {
-    next(error);
+    // next(error);
   }
 })
 
 class Forecast {
   constructor(weatherObject) {
-    this.date = weatherObject.date;
-    this.desc = weatherObject.desc;
+    this.date = weatherObject.datetime;
+    this.desc = weatherObject.weather.description;
   }
 }
 
 //Errors
-
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+})
 //Listen
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
